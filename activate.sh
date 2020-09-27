9b07b35ff2a0a10e4f6fbf9c4ef29d2411035e78
nobodyzxc <nobodyzxc.tw@gmail.com>
2020-09-27 23:56:00 +0800

    update activate scripts

--- activate.sh
+++ activate.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -w /root -v ${PWD}:/root/hw1 plaslab/compiler-f20-hw1:latest /bin/bash
+docker run --rm -it -w /root -v "${PWD}":/root/hw1 plaslab/compiler-f20-hw1:latest /bin/bash
