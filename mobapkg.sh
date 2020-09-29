b40adcb931abaea25269d37a27836b84a282e180
U-PCAD\zxchen <zxchen@CSCC-31636.pcad.cs.nctu.edu.tw>
2020-09-29 12:36:19 +0800

    add zip

--- mobapkg.sh
+++ mobapkg.sh
@@ -2,4 +2,5 @@ yes | /bin/MobaBox.exe apt-get install make
 yes | /bin/MobaBox.exe apt-get install flex
 yes | /bin/MobaBox.exe apt-get install gcc-core
 yes | /bin/MobaBox.exe apt-get install m4
+yes | /bin/MobaBox.exe apt-get install zip
 #yes | /bin/MobaBox.exe apt-get apt-get install bison
