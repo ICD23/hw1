e244a7e318041d6358c16d33bb3ba9613e6dc4e1
U-PCAD\zxchen <zxchen@CSCC-31636.pcad.cs.nctu.edu.tw>
2020-09-29 12:31:21 +0800

    update pkg script

--- mobapkg.sh
+++ mobapkg.sh
@@ -1,5 +1,5 @@
-yes | /bin/MobaBox.exe apt-get apt-get install make
-yes | /bin/MobaBox.exe apt-get apt-get install flex
+yes | /bin/MobaBox.exe apt-get install make
+yes | /bin/MobaBox.exe apt-get install flex
+yes | /bin/MobaBox.exe apt-get install gcc-core
+yes | /bin/MobaBox.exe apt-get install m4
 #yes | /bin/MobaBox.exe apt-get apt-get install bison
-yes | /bin/MobaBox.exe apt-get apt-get install gcc-core
-yes | /bin/MobaBox.exe apt-get apt-get install m4
